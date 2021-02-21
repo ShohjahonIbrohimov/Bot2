9e48d188bbc380a7700f0d70b6d1f314bba360f1
ShohjahonIbrohimov <cleverboy03.06.02@gmail.com>
2021-02-21 12:18:53 +0500

    FIXED: routes; CHANGE: sidebar->header->bg

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Layout, Menu } from 'antd';
 import {
     MenuUnfoldOutlined,
@@ -10,51 +10,53 @@ import {
 
 const { Header, Sider, Content } = Layout;
 
-const SiderDemo = ({ children }) => {
-    const [collapsed, setcollapsed] = useState(false)
-
-    const toggle = () => {
-        setcollapsed(!collapsed)
+export class SiderDemo extends React.Component {
+    state = {
+        collapsed: false,
     };
 
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
 
-    return (
-        <Layout>
-            <Sider trigger={null} collapsible collapsed={collapsed}>
-                <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1" icon={<UserOutlined />}>
-                        nav 1
+    render() {
+        return (
+            <Layout>
+                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1" icon={<UserOutlined />}>
+                            nav 1
             </Menu.Item>
-                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                        nav 2
+                        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                            nav 2
             </Menu.Item>
-                    <Menu.Item key="3" icon={<UploadOutlined />}>
-                        nav 3
+                        <Menu.Item key="3" icon={<UploadOutlined />}>
+                            nav 3
             </Menu.Item>
-                </Menu>
-            </Sider>
-            <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }}>
-                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                        className: 'trigger',
-                        onClick: toggle,
-                    })}
-                </Header>
-                <Content
-                    className="site-layout-background"
-                    style={{
-                        margin: '24px 16px',
-                        padding: 24,
-                        minHeight: 280,
-                    }}
-                >
-                    {children}
-                </Content>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header className="site-layout-background" style={{ background: "#fff" }}>
+                        {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                            className: 'trigger',
+                            onClick: this.toggle,
+                        })}
+                    </Header>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            minHeight: 280,
+                        }}
+                    >
+                        {this.props.children}
+                    </Content>
+                </Layout>
             </Layout>
-        </Layout>
-    );
-
+        );
+    }
 }
-
-export default SiderDemo;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
 import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+import '../css/main.css';
 import 'antd/dist/antd.css';
-import '../css/main.css'
-import SiderDemo from "../components/Layout";
+import { SiderDemo } from "../components/Layout";
 import { ROUTES, ROUTES_SIGN_IN_SIGN_UP } from "../routes/routes";
 import { store } from "../redux/store";
 import axios from 'axios';
+import { Redirect } from "react-router-dom";
 // REDUX
 import {
     selectToken
 } from "../redux/auth/auth.selector";
-
 import { createStructuredSelector } from "reselect";
 import { connect } from "react-redux";
 
@@ -20,14 +20,18 @@ const MainPage = ({ token }) => {
     axios.defaults.baseURL = "https://api.apicrm.online/";
     axios.defaults.headers.common['x-access-token'] = token
     return (
-        <SiderDemo>
-            <Switch>
-                {ROUTES.map((route) => (
-                    <Route {...route} />
-                ))}
-                <ScreenSignIn />
-            </Switch>
-        </SiderDemo>
+        <>
+            {!token && <Redirect to="/login" />}
+            {token && <SiderDemo>
+                <Switch>
+
+                    {ROUTES.map((route) => (
+                        <Route {...route} />
+                    ))}
+                </Switch>
+            </SiderDemo>}
+            <ScreenSignIn />
+        </>
     );
 };
 
